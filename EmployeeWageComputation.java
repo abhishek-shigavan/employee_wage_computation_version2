74561f2d8fc7f1e1b3df3f812a9125fa1e92ba76
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-10 19:49:45 +0530

    Add: Attendance checker added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,9 +1,43 @@
+/**
+ * EmployeeWageComputation 
+ * 		--- Program to compute employee wage.
+ * 
+ * Checking attendance (i.e present / absent)
+ * 
+ * 
+ * @author    Abhishek Shigavan
+ */
 public class EmployeeWageComputation {
+
+	//constant
+	public static final int IS_PRESENT=1;
+	
+/**
+   * Generate random attendance 
+   * and store it in variable
+	   * @return return attendance.
+	   * 
+	   *
+   */	
+	public static int attendanceChecker() {
+		
+		int attendance = (int)(Math.floor(Math.random() * 10) % 2);
+		return attendance;
+	}
 	
 	public static void main(String[] args) {
 	
 		System.out.println("Welcome To Employee Wage Computation...!!!");
 
+		int empAttendance = attendanceChecker();
+		
+		if(empAttendance == IS_PRESENT) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
+
