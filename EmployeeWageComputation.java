23636b8b621c36963c6c3b420028f635ce5a97a5
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-10 20:13:10 +0530

    Add: Computing monthly wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,6 +4,7 @@
  * 
  * Checking attendance (i.e present / part time / absent)
  * Computing daily wage of employee
+ * Computing monthly wage of employee
  * 
  * 
  * @author    Abhishek Shigavan
@@ -14,7 +15,7 @@ public class EmployeeWageComputation {
 	public static final int IS_PRESENT=1;
 	public static final int IS_PART_TIEM=2;
 	public static final int WAGE_PER_HOUR=20;
-
+	public static final int WORKING_DAY_PER_MONTH=20;
 /**
    * Generate random attendance 
    * (i.e present / part time / absent)
@@ -43,31 +44,34 @@ public class EmployeeWageComputation {
 	
 	public static void main(String[] args) {
 	
-		//variables
-		int empWorkingHour=0;
-		int empAttendance = attendanceChecker();
+		int monthlyWage=0;
 		
-		System.out.println("Welcome To Employee Wage Computation...!!!");		
-
-		switch(empAttendance) {
-		case IS_PRESENT: {
-			empWorkingHour=8;
-			System.out.println("Employee is Present");
-			break;
-		}
-		case IS_PART_TIEM: {
-			empWorkingHour=4;
-			System.out.println("Employee is Part Time Present");
-			break;
-		}
-		default:
-			System.out.println("Employee is Absent");
-			break;
-		}
+		System.out.println("Welcome To Employee Wage Computation...!!!");
 		
-		int empDailyWage=getDailyWage(empWorkingHour);
-		System.out.println("Daily Wage of Employee : "+empDailyWage);
-	}
+		for(int day=1; day<=WORKING_DAY_PER_MONTH; day++) {
+		
+			//variables
+			int empWorkingHour=0;
+			int empAttendance = attendanceChecker();
+		
+			switch(empAttendance) {
+			case IS_PRESENT: {
+				empWorkingHour=8;
+				break;
+			}
+			case IS_PART_TIEM: {
+				empWorkingHour=4;
+				break;
+			}
+			default:
+				break;
+			}
+		
+			int empDailyWage=getDailyWage(empWorkingHour);
+			monthlyWage +=empDailyWage;
+		}
 
+		System.out.println("Monthly Wage of Employee : "+monthlyWage);
+	}	
 }
 
